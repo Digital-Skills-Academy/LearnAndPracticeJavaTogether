c51036abc90e2950e6bfc4a47b317d09df4c698b
Wenhao Fu <wenhao.fu@gmail.com>
2023-06-30 10:17:25 +0100

    Add files via upload

---
+++ Array/ingredientsChecker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+/**
+ * Write a description of class ingredientsChecker here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ingredientsChecker
+{
+   public static void main(String[] args){
+
+
+    String[] ingredients = {"egg","onion","tomato","garlic","butter","chicken"};
+    
+    
+    }
+}
